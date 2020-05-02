3406f03c174db0a1c6f2418a0b12bed3e5bdc9e3
zhaoyin <zhaoyin@corp.netease.com>
2020-05-02 11:10:12 +0800

    工程初始化

---
+++ leetcode/src/main/java/src/leetcode/easy/Test.java
@@ -0,0 +1,13 @@
+package src.leetcode.easy;
+
+/**
+ * @Author zhaoyin@corp.netease.com
+ * @Date 2020/5/2 11:07
+ * @Version 1.0
+ * @Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我额日");
+    }
+}
