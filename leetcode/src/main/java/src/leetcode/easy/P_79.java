abf74df50ed9a2697220d65cd77bd7122c97bd69
zhaoyin <zhaoyin@corp.netease.com>
2020-05-04 10:42:39 +0800

    小作两题

---
+++ leetcode/src/main/java/src/leetcode/easy/P_79.java
@@ -0,0 +1,18 @@
+package src.leetcode.easy;
+
+/**
+ * @Author zhaoyin@corp.netease.com
+ * @Date 2020/5/4 10:37
+ * @Version 1.0
+ * @Description 单词搜索
+ */
+public class P_79 {
+
+    public static void main(String[] args) {
+
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+
+    }
+
+
+}
