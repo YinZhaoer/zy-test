3c2ef3322987791557df40d500e8bdf66826ef7e
zhaoyin <zhaoyin@corp.netease.com>
2020-05-07 09:29:33 +0800

    MOD:实现lru

---
+++ leetcode/src/main/java/src/leetcode/medium/LRUCache.java
@@ -0,0 +1,116 @@
+package src.leetcode.medium;
+
+import java.util.Hashtable;
+
+/**
+ * @author: ZhaoYin
+ * @version: 2020/5/7
+ * @description: P_146构造LRU缓存
+ * 1.直接利用有序字典 linkedHashMap 2.使用HashMap（实现get put的O(1)）+linkedList（充当节点，实现最远淘汰）自己实现（其实就是1的简单实现）
+ */
+public class LRUCache {
+
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+    }
+
+    private void addNode(DLinkedNode node) {
+        /**
+         * Always add the new node right after head.
+         */
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        /**
+         * Remove an existing node from the linked list.
+         */
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        /**
+         * Move certain node in between to the head.
+         */
+        removeNode(node);
+        addNode(node);
+    }
+
+    private DLinkedNode popTail() {
+        /**
+         * Pop the current tail.
+         */
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    private Hashtable<Integer, DLinkedNode> cache =
+            new Hashtable<Integer, DLinkedNode>();
+    private int size;
+    private int capacity;
+    //使用两个伪节点充当头尾，减少null判断
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        // head.prev = null;
+
+        tail = new DLinkedNode();
+        // tail.next = null;
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+
+        // move the accessed node to the head;
+        moveToHead(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            cache.put(key, newNode);
+            addNode(newNode);
+
+            ++size;
+
+            if (size > capacity) {
+                // pop the tail
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            // update the value.
+            node.value = value;
+            moveToHead(node);
+        }
+
+    }
+}
